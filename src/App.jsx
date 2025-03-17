6fc6e8839f84c629d350c17eb441c35f01b70b23
Mohamed Khaled <mohamedkhaled329@yahoo.com>
2025-03-17 12:49:08 +0200

    udate

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./Component/Header/Header";
+
+
+const App = ()  => {
+  return (
+    <>
+        <Header/>
+    </>
+  );
+}
+  
+
+
+export default App;
---
+++ src/Component/Header/Header.jsx
@@ -0,0 +1,12 @@
+import Header from "./Header";
+
+function App() {
+  return (
+    <div>
+      
+      <p>Welcome to my app!</p>
+    </div>
+  );
+}
+
+export default App;
